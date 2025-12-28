d1316ed5daeb0882eb197646c8f68447c38861f1
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-12-28 14:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	if (s.length === 0){
+		return s;
+	}
+	const stp=s.indexOf(" ");
+	if(stp === -1){
+		return s;
+	}
+	return s.slice(0, stp);
 }
 
 // Do not change the code below
